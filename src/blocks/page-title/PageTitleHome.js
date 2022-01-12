d4276bdc5ed6d28a4751737417eed79d074f8fe6
DevSoft <devonvanw@gmail.com>
2022-01-12 10:11:09 +0100

    First

--- src/blocks/page-title/PageTitleHome.js
+++ src/blocks/page-title/PageTitleHome.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const PageTitleHome = () => {
-    return (
-        <section id="page-title" className="block">
-            <div className="wrapper">
-                <div className="page-title-content">
-                    <div className="page-title-img">
-                        <img src="assets/img/demo/03_img.png" alt="Happy Anniversary" />
-                    </div>
+  return (
+    <section id="page-title" className="block">
+      <div className="wrapper">
+        <div className="page-title-content">
+          <div className="page-title-img">
+            <img src="assets/img/demo/01_img.png" alt="Home camera" />
+          </div>
 
-                    <div className="page-title-description">
-                        <div className="title">
-                            <h1 className="large">Happy<br/>Anniversary.</h1>
-                        </div>
-
-                        <div className="description">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        </div>
-                    </div>
-                </div>
+          <div className="page-title-description">
+            <div className="title">
+              <h1 className="large">
+                Happy
+                <br />
+                Anniversary.
+              </h1>
             </div>
-        </section>
-    );
+
+            <div className="description">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PageTitleHome;
